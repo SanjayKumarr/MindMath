59b087388c6c2588e2f8161ae450efa6b150dad2
Sanjay Kumar <sanjaykumarap18@gmail.com>
2017-08-21 09:29:00 +0530

    Menu addition

--- js/menu.js
+++ js/menu.js
@@ -13,27 +13,37 @@ var menuState = {
     
     var btn_text_style = {fill: '#fff', font: "20px"};
 
-    var help_tux_btn = game.add.button(game.world.centerX, 500, 'text_button', startHomeWork);
+    var help_tux_btn = game.add.button(game.world.centerX, 500, 'text_button', this.startHomeWork);
     help_tux_btn.anchor.setTo(0.5);
     var help_tux_txt = game.add.text(game.world.centerX, 500, 'Help Tux\'s H/w', btn_text_style);
     help_tux_txt.anchor.setTo(0.5);
 
-    var show_menu_btn = game.add.button(game.world.centerX, 600, 'text_button', startHomeWork);
+    var show_menu_btn = game.add.button(game.world.centerX, 700, 'text_button', this.moveToTricksList);
     show_menu_btn.anchor.setTo(0.5);
-    var show_menu_txt = game.add.text(game.world.centerX, 600, 'Show all tricks', btn_text_style);
+    var show_menu_txt = game.add.text(game.world.centerX, 700, 'Show all tricks', btn_text_style);
     show_menu_txt.anchor.setTo(0.5);
 
-    var calc_challenge_btn = game.add.button(game.world.centerX, 700, 'text_button', startHomeWork);
+    var calc_challenge_btn = game.add.button(game.world.centerX, 600, 'text_button', this.startCalcChallenge);
     calc_challenge_btn.anchor.setTo(0.5);
-    var calc_challenge_txt = game.add.text(game.world.centerX, 700, 'Calculator challenge', btn_text_style);
+    var calc_challenge_txt = game.add.text(game.world.centerX, 600, 'Calculator challenge', btn_text_style);
     calc_challenge_txt.anchor.setTo(0.5);
 
-
     //game.state.start('questionDisplay');
+  },
+
+  moveToTricksList: function() {
+    game.state.start('tricksList');
+  },
+
+  startCalcChallenge: function() {
+    game.state.start('calcChallenge');
+  },
+
+  startHomeWork: function startHomeWork() {
+    game.state.start('questionDisplay');
+    moving_back = false;
   }
+
 };
 
-function startHomeWork() {
-  game.state.start('questionDisplay');
-  moving_back = false;
-}
+
