313554536368e922618b844805fda33351143b01
Sanjay Kumar <sanjaykumarap18@gmail.com>
2017-08-21 09:29:27 +0530

    Canvas answer field to play js

--- js/play.js
+++ js/play.js
@@ -1,7 +1,7 @@
 var timer_txt, timer, elapsed_seconds = 0;
 var lollipops_count_txt, lollipops_count = 0;
 var question_num, question_txt, answer_helper_left, answer_helper_right;
-var answer_field;
+
 var playState = {
 
   create: function () {
@@ -32,33 +32,16 @@ var playState = {
   game.add.text(game.world.centerX - 400 ,180, "Time", {fill: 'orange', font: "bold 20px"}, helper_texts);
   game.add.text(game.world.centerX + 400 ,180, "Lollipops", {fill: 'orange', font: "bold 20px"}, helper_texts);
 
-  game.add.image(240, 250, 'homework_template');
+  game.add.image(240, 130, 'homework_template');
   var penguin_helper = game.add.sprite(120, 650, 'penguin_helper');
   penguin_helper.scale.setTo(0.35, 0.35);
   game.add.text(220, 800, "I am here to help you anytime!", {fill: '#fff', font: "bold 20px"}, helper_texts);
 
-  question_txt = game.add.text(game.world.centerX ,320, '( ' + 0 + ' )', {fill: 'black', font: 'bold 30px'}, helper_texts);
-  answer_helper_left = game.add.text(game.world.centerX - 250 ,480, '1 * Next number to 1', {fill: '#ccc', font: '20px'}, helper_texts);
-  answer_helper_right = game.add.text(game.world.centerX + 250 ,480, '25', {fill: '#ccc', font: '20px'}, helper_texts);
-  answer_helper_middle = game.add.text(game.world.centerX ,480, '', {fill: '#ccc', font: '20px'}, helper_texts);
-  answer_field = game.add.inputField(game.world.centerX - 70,620, {
-    font: '25px',
-    fill: '#000',
-    fillAlpha: 0,
-    fontWeight: 'bold',
-    width: 150,
-    padding: 10,
-    borderWidth: 0,
-    placeHolder: '',
-    type: PhaserInput.InputType.number,
-    zoom: false
-  });
-  answer_field.anchor.setTo(0.5);
-  answer_field.blockInput = false
-  answer_field.startFocus();
-
-  enter_key = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
-  enter_key.onDown.add(verifyAnswer, this);
+  question_txt = game.add.text(game.world.centerX ,200, '( ' + 0 + ' )', {fill: 'black', font: 'bold 30px'}, helper_texts);
+  answer_helper_left = game.add.text(game.world.centerX - 250 ,360, '1 * Next number to 1', {fill: '#ccc', font: '20px'}, helper_texts);
+  answer_helper_right = game.add.text(game.world.centerX + 250 ,360, '25', {fill: '#ccc', font: '20px'}, helper_texts);
+  answer_helper_middle = game.add.text(game.world.centerX ,360, '', {fill: '#ccc', font: '20px'}, helper_texts);
+  setInputField(true);
 
   helper_texts.setAll('anchor.x', 0.5);
   helper_texts.setAll('anchor.y', 0.5);
@@ -78,8 +61,36 @@ function updateCounter() {
 }
 
 function startGame() {
+  var canvas_element = document.getElementById('canvas');
+  canvas_element.style.visibility = "visible";
+  canvas_element.addEventListener("click", getFocusOnAnswerField);
+  elapsed_seconds = 0;
+  lollipops_count = 0;
   setQuestion();
 
+}
+
+function setInputField(placeholder_text_condition) {
+  answer_field = new CanvasInput({
+    canvas: document.getElementById('canvas'),
+    fontSize: 18,
+    fontFamily: 'Arial',
+    fontColor: '#212121',
+    fontWeight: 'bold',
+    width: 200,
+    height: 30,
+    padding: 8,
+    borderWidth: 0,
+    borderColor: '#000',
+    borderRadius: 3,
+    //boxShadow: '1px 1px 0px #fff',
+    //innerShadow: '0px 0px 5px rgba(0, 0, 0, 0.5)',
+    placeHolder: (placeholder_text_condition) ? '         Your answer': '         Try again!',
+    textAlign: 'right',
+    onsubmit: function() {
+      verifyAnswer();
+    },
+  });
 }
 
 function setQuestion() {
@@ -89,17 +100,18 @@ function setQuestion() {
     question_num = Math.round(Math.random() * 9) * 10 + 5;
     var first_digit = Math.floor(question_num/10);
 
-    question_txt.setText('( ' + question_num + ' )^2');
+    question_txt.setText('( ' + question_num + ' )²');
     answer_helper_left.setText(first_digit + ' * Next number to ' + first_digit + '  (' + (++first_digit) + ')');    
     answer_helper_right.setText('5 * 5');
+    answer_helper_middle.setText('');
     break;
 
     case 1:
     //Square of numbers just below 100
     question_num = getRandomInt(91, 96);
 
-    question_txt.setText('( ' + question_num + ' )^2');
-    answer_helper_middle.setText('Difference of '+ question_num +' with 100 ('+ (100 - question_num) + ')');
+    question_txt.setText('( ' + question_num + ' )²');
+    answer_helper_middle.setText('');
     answer_helper_left.setText(question_num +' - '+ (100 - question_num));
     answer_helper_right.setText('Sqaure of '+ (100 - question_num));
     break;
@@ -111,7 +123,7 @@ function setQuestion() {
     var second_digit = Math.floor(question_num%10);
 
     question_txt.setText('( ' + question_num + ' ) * 9');
-    answer_helper_middle.setText('The Number next to '+ first_digit + '. (' + (first_digit + 1) +')');
+    answer_helper_middle.setText('');
     answer_helper_left.setText(question_num +' - '+ (first_digit + 1));
     answer_helper_right.setText('10 - '+ second_digit);
     break;
@@ -119,8 +131,8 @@ function setQuestion() {
     // sqaure of number just above 100
     question_num = getRandomInt(104, 109);
 
-    question_txt.setText('( ' + question_num + ' )^2');
-    answer_helper_middle.setText('Difference of 100 with '+ question_num + ' ('+ (question_num - 100) + ')');
+    question_txt.setText('( ' + question_num + ' )²');
+    answer_helper_middle.setText('');
     answer_helper_left.setText(question_num +' + '+ (question_num - 100));
     answer_helper_right.setText('Sqaure of '+ (question_num - 100));
     break;
@@ -131,7 +143,7 @@ function setQuestion() {
     var first_digit = Math.floor(question_num/100);
     var last_digit = Math.floor(question_num%10);
 
-    question_txt.setText('( ' + question_num + ' )^2'); 
+    question_txt.setText('( ' + question_num + ' )²'); 
     answer_helper_middle.setText('(' + first_digit +') * ('+ last_digit + ') * 2');
     answer_helper_left.setText('Square of '+ first_digit);
     answer_helper_right.setText('Sqaure of '+ last_digit);
@@ -142,9 +154,9 @@ function setQuestion() {
     question_num = getRandomInt(10, 49) * 2;
 
     question_txt.setText('( ' + question_num + ' ) * 5'); 
-    answer_helper_middle.setText('('+ (question_num/2) +' * 10)');
-    answer_helper_left.setText('Find half of '+ question_num);
-    answer_helper_right.setText('Half ('+ (question_num/2)+ ') * 10');
+    answer_helper_middle.setText('Half of '+ question_num + ' * 10');
+    answer_helper_left.setText('');
+    answer_helper_right.setText('');
     break;
 
     case 6:
@@ -190,51 +202,52 @@ function setQuestion() {
 
 function verifyAnswer() {
   var answered_right = false;
+  var user_answer = (parseInt(answer_field.value()) != NaN) ? parseInt(answer_field.value()) : 0
   switch(chosen_question_number) {
     case 0:
-    if (parseInt(answer_field.value) === question_num * question_num) {
+    if (user_answer === question_num * question_num) {
       answered_right = true;
     }
     break;
 
     case 1:
-    if (parseInt(answer_field.value) === question_num * question_num) {
+    if (user_answer === question_num * question_num) {
       answered_right = true;
     }
     break;
 
     case 2:
-    if (parseInt(answer_field.value) === question_num * 9) {
+    if (user_answer === question_num * 9) {
       answered_right = true;
     }
     break;
 
     case 3:
-    if (parseInt(answer_field.value) === question_num * question_num) {
+    if (user_answer === question_num * question_num) {
       answered_right = true;
     }
     break;
 
     case 4:
-    if (parseInt(answer_field.value) === question_num * question_num) {
+    if (user_answer === question_num * question_num) {
       answered_right = true;
     }
     break;
 
     case 5:
-    if (parseInt(answer_field.value) === question_num * 5) {
+    if (user_answer === question_num * 5) {
       answered_right = true;
     }
     break;
 
     case 6:
-    if (parseInt(answer_field.value) === question_num * 11) {
+    if (user_answer === question_num * 11) {
       answered_right = true;
     }
     break;
 
     case 7:
-    if (parseInt(answer_field.value) === question_num[0] * question_num[1]) {
+    if (user_answer === question_num[0] * question_num[1]) {
       answered_right = true;
     }
     break;
@@ -242,21 +255,18 @@ function verifyAnswer() {
     default:
     break;
   }
+  
   if (answered_right) {
     lollipops_count = lollipops_count + 10;
     lollipops_count_txt.setText(lollipops_count);
-    startGame();
-    answer_field.setText("Correct!, Next.");
-    answer_field.startFocus(); 
-    answer_field.update();
-
+    if(lollipops_count >= 100) {
+      game.state.start('end');
+    }
+    setQuestion();
   }
-  else {
-    answer_field.setText("Try again!");
-    answer_field.startFocus(); 
-    answer_field.update();
-  }
-  
+  answer_field.destroy();
+  setInputField(answered_right);
+  answer_field.focus();  
 }
 
 function getRandomInt(min, max) {
@@ -266,7 +276,13 @@ function getRandomInt(min, max) {
 function goBackFunction() {
   game.state.start('questionDisplay');
   moving_back = true;
+  answer_field.destroy();
+  document.getElementById('canvas').style.visibility = "hidden";
 }
 
-
+function getFocusOnAnswerField() {
+  if(typeof(answer_field) != 'undefined'){
+    answer_field.focus();
+  }
+}
 
